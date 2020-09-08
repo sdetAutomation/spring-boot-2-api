88458cb2d6b356fcac4a5e91a67da58a4474b810
sdetAutomation <sdet.testautomation@gmail.com>
2020-09-08 12:22:29 -0500

    test: update spring contextloads test to include an assert

--- src/test/java/com/sdet/auto/springboot2api/SpringBoot2ApiApplicationTests.java
+++ src/test/java/com/sdet/auto/springboot2api/SpringBoot2ApiApplicationTests.java
@@ -1,16 +1,23 @@
 package com.sdet.auto.springboot2api;
 
+import com.sdet.auto.springboot2api.controller.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBoot2ApiApplicationTests {
 
+	@Autowired
+	private HelloController controller;
+
 	@Test
-	public void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertNotNull(controller);
 	}
 
 }
